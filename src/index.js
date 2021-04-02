5758db5e1643cbff685cfc04dde68d2a7650b87c
github username <github email address>
2021-04-02 12:51:09 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form#create-task-form').addEventListener('submit', function(event) {
+    console.log(event);
+    let input = document.querySelector('input#new-task-description').value;
+    let clear = document.querySelector('input#new-task-description').value = '';
+    if (input === '') {
+      alert("Empty Box")
+    } else {
+    document.getElementById('tasks').innerHTML += `${input} <button color='red'>x</button><br>`
+    clear
+
+    };
+    event.preventDefault();
+  }, false); 
+
 });
+
